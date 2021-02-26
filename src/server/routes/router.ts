df0c1c6cf4695a6047e9d03f24075c4af660d14a
Pietro Bondioli <pietrobondiolideveloper@gmail.com>
2021-02-26 20:15:01 -0300

    feat: created 'workout' table Controller, added api routes in the 'router', updated 'render' methods

--- src/server/routes/router.ts
+++ src/server/routes/router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as render from '../services/render';
+import * as workoutController from '../controller/workoutController';
 
 const router = express.Router();
 
@@ -8,5 +9,10 @@ router.get('/', render.home);
 router.get('/add-workout', render.add_workout);
 
 router.get('/update-workout', render.update_workout);
+
+router.get('/api/workout', workoutController.find);
+router.post('/api/workout', workoutController.create);
+router.post('/api/workout/:id', workoutController.findByIdAndUpdate);
+router.delete('/api/workout/:id', workoutController.findByIdAndDelete);
 
 export default router
--- src/server/services/render.ts
+++ src/server/services/render.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
+import PORT from '../config/port';
 
 const home = (req: Request, res: Response) => {
-  res.render('index');
+  axios
+    .get(`http://localhost:${PORT}/api/workout`)
+    .then((response) => {
+      res.render('index', { workout_list: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 const add_workout = (req: Request, res: Response) => {
@@ -9,7 +18,14 @@ const add_workout = (req: Request, res: Response) => {
 };
 
 const update_workout = (req: Request, res: Response) => {
-  res.render('update_workout');
+  axios
+    .get(`http://localhost:${PORT}/api/workout`, { params: { id: req.query.id } })
+    .then((response) => {
+      res.render('update_workout', { workout: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 export { home, add_workout, update_workout };
