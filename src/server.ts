e10b07980faa3fe10a4954747cf691aa4bc17533
Pietro Bondioli <pietrobondiolideveloper@gmail.com>
2021-02-26 20:06:12 -0300

    feat: add 'sqlite3' module, create 'port.js' to handle default app PORT, add script to 'home'
    - Add 'sqlite3' module for it to be used as the application database
    - Create 'port.js' to decouple 'default app PORT' handling
    from other files
    - Create 'home.js' to handle 'delete_button' action on click
    - Remove 'id' column from home 'container_table'
    - Rename '.env' file to 'config.env'

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,25 @@
+import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
 import morgan from 'morgan';
 import bodyparser from 'body-parser';
-import path from 'path';
 import router from './server/routes/router';
+import PORT from './server/config/port';
 
+// APP
 const app = express();
 
-dotenv.config({ path: '/.env' });
-const PORT = process.env.PORT || 8080;
-
-app.use(morgan('tiny'));
+// MIDDLEWARE
+// app.use(morgan('tiny'));
 app.use(bodyparser.urlencoded({ extended: true }));
 
+// TEMPLATE ENGINE
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
 
+// STATIC PATHS
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
 
+// ROUTES
 app.use('/', router);
 
 app.listen(PORT, () => {
